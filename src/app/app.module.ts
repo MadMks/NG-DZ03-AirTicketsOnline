c97fe638bf84a5cec2e5d7be455b0264bf83c443
Makc <mks59k@gmail.com>
2019-07-05 23:10:40 +0300

    Первый каркас

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,18 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { Test1Component } from './components/test1/test1.component';
+import { TicketComponent } from './components/ticket/ticket.component';
+import { TicketsComponent } from './components/tickets/tickets.component';
+import { SidebarComponent } from './components/sidebar/sidebar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    Test1Component,
+    TicketComponent,
+    TicketsComponent,
+    SidebarComponent
   ],
   imports: [
     BrowserModule
