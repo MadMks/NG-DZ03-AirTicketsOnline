91c59889b53b68877ddc2c2c63d54d5663a8845c
Makc <mks59k@gmail.com>
2019-07-11 22:25:57 +0300

    Начало реализации фильтра билетов (тестирование получения значений чекбоксов), без компонентов и директив

--- src/app/components/sidebar/transfer-filter/checkbox/checkbox.component.ts
+++ src/app/components/sidebar/transfer-filter/checkbox/checkbox.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, EventEmitter, Input, OnInit, Output} from '@angular/core';
 
 @Component({
   selector: 'app-checkbox',
@@ -7,9 +7,17 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CheckboxComponent implements OnInit {
 
+  @Input() numberCheckbox;
+  @Output() checkEvent = new EventEmitter<boolean>();
+  checkboxes: any;
+
   constructor() { }
 
   ngOnInit() {
+  }
+
+  check() {
+    this.checkEvent.emit(true);
   }
 
 }
--- src/app/components/sidebar/transfer-filter/transfer-filter.component.ts
+++ src/app/components/sidebar/transfer-filter/transfer-filter.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 
 @Component({
   selector: 'app-transfer-filter',
@@ -8,14 +8,49 @@ import { Component, OnInit } from '@angular/core';
 export class TransferFilterComponent implements OnInit {
 
   private isNeedToFilter = false;
+  private checkboxValues: boolean[];
 
-  constructor() { }
+  constructor() {
+    this.checkboxValues = [
+      false,
+      false,
+      false,
+      false,
+      false
+    ];
+  }
 
   ngOnInit() {
   }
 
-  checkFilterValues() {
-    console.log(this.isNeedToFilter);
+  // checkFilterValues($event) {
+  //   this.isNeedToFilter = $event;
+  //   console.log(this.isNeedToFilter);
+  // }
+
+  checkFilterValues($event) {
+    // console.log($event.target.checked + ' ' + $event.target.name);
+    // Получение измененного значения чекбокса,
+    // и запись в соответствующее место в массиве
+    this.checkboxValues[$event.target.name.toString()]
+      = $event.target.checked;
+    // Изменение вывода билетов, на основе значений чекбоксов
+    this.changeTicketDisplay();
+    console.log();
+    console.log(this.checkboxValues);
+  }
+
+  changeTicketDisplay() {
+    // Если все false (флажки сняты)
+    if (!this.checkboxValues.includes(true, 0)) {
+      // TODO: показать все билеты
+      console.log('all false');
+    } else if (this.checkboxValues[0] === true
+      && !this.checkboxValues.includes(true, 1)) {
+      // TODO: показать все билеты
+      // TODO: выбрать все флажки
+      console.log('show all');
+    }
   }
 
 }
